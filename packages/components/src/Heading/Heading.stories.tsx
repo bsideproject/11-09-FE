ab68e6362acf91e11d739ca806987982fd9c9158
r0o0 <rayeoh7@gmail.com>
2022-08-06 23:39:54 +0900

    Fix Heading stories based on project's convention

--- packages/components/src/Heading/Heading.stories.tsx
+++ packages/components/src/Heading/Heading.stories.tsx
@@ -3,16 +3,15 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Heading from './Heading';
 
 export default {
-  title: 'Example/Heading',
+  title: 'UI/Heading',
   component: Heading,
   argTypes: { as: { control: 'select' } },
 } as ComponentMeta<typeof Heading>;
 
 const Template: ComponentStory<typeof Heading> = (args) => <Heading {...args} />;
 
-export const Primary = Template.bind({});
-
-Primary.args = {
+export const Base = Template.bind({});
+Base.args = {
   heading: '타임레터 평창평화체',
   as: 'h2',
   size: 4,
