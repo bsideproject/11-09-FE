287cac5a634259d55fe1171a18a1c9dbc7b23906
st <53326835+chang-st@users.noreply.github.com>
2022-09-17 09:07:25 +0900

    * respons 변경에따른 타입 및 api수정

--- packages/app/src/api/api-schema.d.ts
+++ packages/app/src/api/api-schema.d.ts
@@ -41,11 +41,9 @@ declare namespace APISchema {
     password?: string;
     phoneNumber?: string;
   }
-
-  interface ReminderType { data: Letter[]; }
- 
+  
   interface ReminderUpDateType {
-    id?: string;
-    receivedPhoneNumber?: string;
+    letterId?: string;
+    isSended?: boolean
   }
 }
